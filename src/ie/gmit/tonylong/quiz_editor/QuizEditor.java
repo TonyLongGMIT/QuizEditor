8817f88b193d7a8d7bfca253047cfd34b3c1075a
Tony Long <g00235704@gmit.ie>
2012-11-24 18:20:26 +0000

    Initial commit.

---
+++ src/ie/gmit/tonylong/quiz_editor/QuizEditor.java
@@ -0,0 +1,14 @@
+package ie.gmit.tonylong.quiz_editor;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class QuizEditor extends JFrame {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
